da40ca372a17622e81857c80bd58fec0315fe38d
wucuiping <1061814032@qq.com>
2021-05-16 23:03:25 +0800

    工程目录、服务注册

---
+++ ch04/main.go
@@ -0,0 +1,44 @@
+package main
+
+import(
+	"fmt"
+	"net"
+	"net/http"
+	"log"
+	"net/rpc"
+	server "./server"
+	
+)
+
+func main() {
+	http.HandleFunc("/", home) //DefaultServeMutex
+	//start a http server
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	//register a rpc service
+	arith := new(server.Arith)
+	rpc.Register(arith)
+	rpc.HandleHTTP()
+	l, e := net.Listen("tcp", ":9090")
+	if e != nil {
+		log.Fatal("rpc listen error:", e)
+	}
+	go http.Serve(l, nil)
+
+	client, err := rpc.DialHTTP("tcp", "localhost" + ":9090")
+	if err != nil {
+		log.Fatal("dialing: ", err)
+	}
+
+	args := &server.Args{7, 8}
+	var reply int
+	err = client.Call("Arith.Multiply", args, &reply)
+	if err != nil {
+		log.Fatal("arith error: ", err)
+	}
+	fmt.Printf("Arith: %d*%d=%d", args.A, args.B, reply)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to RPC world")
+}
